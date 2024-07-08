26aaa993ab26875fadcd0f3016c346560458fc3d
rgaldames <ri.galdames@duocuc.cl>
2024-07-08 18:48:36 -0400

    Inicio

---
+++ funciones.py
@@ -0,0 +1,8 @@
+
+def menues():
+    print("menu")
+    print("1.- Agregar")
+    print("2.- Eliminar")
+    print("3.- Buscar")
+
+
---
+++ inicio.py
@@ -0,0 +1,10 @@
+import funciones
+
+
+def main():
+    op=funciones.menues()
+
+
+if __name__='__main__':
+    main
+   
